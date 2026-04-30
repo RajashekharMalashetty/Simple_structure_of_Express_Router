59567e3dc7737419d54ca368ff5afda7b20d1627
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-04-30 12:12:58 +0530

    Sending Cookies

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
+app.get("/getcookies", (req, res) => {
+    res.cookie("greet", "hello");
+    res.send("sent you some cookies!");
+});
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root!");
 });
