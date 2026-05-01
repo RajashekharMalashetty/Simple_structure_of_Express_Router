e36d25d317c35d9ae4be7ca6fce69086bf5b408b
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-05-02 00:16:24 +0530

    Express Session

--- server.js
+++ server.js
@@ -2,38 +2,48 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 
-app.use(cookieParser("secretcode"));
+// app.use(cookieParser("secretcode"));
 
-app.get("/getsignedcookie", (req, res) => {
-    res.cookie("made-in", "India", {signed: true});
-    res.send("signed cookie sent");
+// app.get("/getsignedcookie", (req, res) => {
+//     res.cookie("made-in", "India", {signed: true});
+//     res.send("signed cookie sent");
+// });
+
+// app.get("/verify", (req, res) => {
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// });
+
+// app.get("/getcookies", (req, res) => {
+//     res.cookie("greet", "hello");
+//     res.cookie("madeIn", "India");
+//     res.send("sent you some cookies!");
+// });
+
+// app.get("/greet", (req, res) => {
+//     let {name = "anonymous"} = req.cookies;
+//     res.send(`Hi, ${name}`);
+// })
+
+// app.get("/", (req, res) => {
+//     console.dir(req.cookies);
+//     res.send("Hi, I am root!");
+// });
+
+// app.use("/users", users);
+// app.use("/posts", posts);
+
+const session = require("express-session");
+
+app.use(session({secret: "mysupersecretstring"}));
+
+app.get("/test", (req, res) => {
+    res.send("test successful!");
 });
 
-app.get("/verify", (req, res) => {
-    console.log(req.signedCookies);
-    res.send("verified");
-});
-
-app.get("/getcookies", (req, res) => {
-    res.cookie("greet", "hello");
-    res.cookie("madeIn", "India");
-    res.send("sent you some cookies!");
-});
-
-app.get("/greet", (req, res) => {
-    let {name = "anonymous"} = req.cookies;
-    res.send(`Hi, ${name}`);
-})
-
-app.get("/", (req, res) => {
-    console.dir(req.cookies);
-    res.send("Hi, I am root!");
-});
-
-app.use("/users", users);
-app.use("/posts", posts)
+ 
 
 app.listen(3000, () => {
     console.log("server is listing to 3000");
