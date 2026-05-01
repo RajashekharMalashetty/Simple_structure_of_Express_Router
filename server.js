956a22f7a958125cc6f8ef87a68f3bc3fc457741
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-05-02 00:29:49 +0530

    Exploring Session Options

--- server.js
+++ server.js
@@ -37,11 +37,20 @@ const posts = require("./routes/post.js");
 
 const session = require("express-session");
 
-app.use(session({secret: "mysupersecretstring"}));
+app.use(session({secret: "mysupersecretstring", resave: false, saveUninitialized: true}));
 
-app.get("/test", (req, res) => {
-    res.send("test successful!");
+app.get("/reqcount", (req, res) => {
+    if(req.session.count){
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You sent a request ${req.session.count} times`);
 });
+
+// app.get("/test", (req, res) => {
+//     res.send("test successful!");
+// });
 
  
 
